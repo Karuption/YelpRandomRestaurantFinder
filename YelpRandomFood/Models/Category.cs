63ba9a719f9e5145571973ccf42b662e4e8d4144
Karuption <pederson26@gmail.com>
2021-11-22 23:07:43 -0600

    Added feature to filter the food categories and only randomly generate between them.
    Need an all check/uncheck feature.
    
    Persist so that they are cached between re-freshes/navigation?

--- YelpRandomFood/Models/Category.cs
+++ YelpRandomFood/Models/Category.cs
@@ -1,10 +1,24 @@
-﻿namespace YelpRandomRestaurantFinder.Data;
+﻿using System.Collections;
 
-public class Category {
+namespace YelpRandomRestaurantFinder.Data;
+
+public class Category : IEqualityComparer {
     public Category() {
 
     }
     public string? Title { get; set; }
     public string alias { get; set; }
     public IList<Category> Parent_Categories { get; set; }
+
+    public new bool Equals(object? x, object? y) {
+        if(x is Category && y is Category)
+            return Equals((Category)x, (Category)y);
+        else
+            return false;
+    }
+    public bool Equals(Category? x, Category? y) => 
+        (x?.Title == y?.Title) && (x?.alias == y?.alias);
+
+    public int GetHashCode(object obj) => 
+        obj.GetHashCode();
 }
